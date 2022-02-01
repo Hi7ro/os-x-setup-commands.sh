fd6d3638a57a97b99ca9f2d85ba303c2b64abb8e
xnro <71623576+Hi7ro@users.noreply.github.com>
2022-02-01 21:15:17 +0100

    Update os-x-setup-commands.sh

--- os-x-setup-commands.sh
+++ os-x-setup-commands.sh
@@ -11,10 +11,11 @@ brew install speedtest-cli
 brew install vcprompt
 # update bash_profile
 brew install --cask lulu #better firewall
-brew cask install spectacle
-brew cask install alfred
+brew install --cask spectacle
+brew install --cask alfred
 # set CMD+space to launch alfred
-brew cask install firefox
+brew install --cask firefox
+brew install --cask istat
 # install nvm/node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 nvm install stable
