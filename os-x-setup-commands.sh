db4558502c88b0cd125aa60abde826adb87e3bf4
xnro <71623576+Hi7ro@users.noreply.github.com>
2022-04-07 00:45:28 +0200

    Update os-x-setup-commands.sh

--- os-x-setup-commands.sh
+++ os-x-setup-commands.sh
@@ -1,7 +1,7 @@
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew cask install iterm2
+brew install --cask iterm2
 # update iterm2 settings -> colors, keep directory open new shell, keyboard shortcuts
 brew install bash # latest version of bash
 # set brew bash as default shell
@@ -21,6 +21,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install stable
 mkdir ~/workspace
 npm install -g lite-server eslint
-brew cask install visual-studio-code
+brew install --cask visual-studio-code
 # update vscode settings
 # install vscode extensions 
